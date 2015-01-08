921bcfca1996171d27900965b9227acd66c98069
Jason Miller <jmillerlaureate@gmail.com>
2015-01-08 11:50:06 -0500

    Added StackOverflow data

--- server.R
+++ server.R
@@ -2,10 +2,12 @@
 ## Desc: This is the ui of my Coursera class project shiny app
 ## Copyright: (c) 2014, Jason D. Miller
 
+require(RCurl)
 require(googleVis)
 library(shiny)
 require(shinyapps)
-
+so   <- getURL("http://wikisend.com/download/725112/stackexchange.csv")
+data <- read.csv(so) 
 shinyServer(function(input, output) {
   output$population <- renderText({paste("The selected population is",
                                          paste(input$select_pop)) })
